4438a017dbbe8567df3b5aec81a2df01f6ec9148
Smedly <bianfuxia@gmail.com>
2025-10-26 08:01:52 -0700

    Add files via upload
    Initial checking for ESM demonstration

---
+++ ESM.py
@@ -0,0 +1,392 @@
+import tkinter as tk
+import random
+import os
+import time
+from pygame import mixer
+
+# Initialize Pygame Mixer
+# 
+# mixer.init()
+
+import time
+
+for i in range(5):
+    try:
+        mixer.init()
+        break
+    except Exception as e:
+        print(f"Audio init failed (attempt {i+1}): {e}")
+        time.sleep(2)
+
+
+home_dir=os.path.expanduser("~")
+esm_dir   = home_dir + "/ESM"
+audio_dir = esm_dir + "/audio"
+image_dir = esm_dir + "/image"
+
+# --- after imports and mixer.init() ---
+
+root = tk.Tk()
+
+# Swear dictionary
+
+swear_dict = {
+    "Anger":    [ "Kurat!",                            "Persevest!",                   "Sitapea!",
+                  "Käi põrgu!",                        "Loll kui saabas!",            "Kanaaju!",
+                  "Jälkus!",                           "Tainapea!",                   "Loll nagu oinas!",
+                  "Käi kuradile!",                     "Kuradi türa!" ],
+    "Humor":    [ "Püha müristus!",                    "Taevas appi!",                "Püha püss!",
+                  "Mine metsa!",                       "Sõida seenele!",              "Tõmba uttu!",
+                  "Käi kuu peale!",                    "Käi kukele!",                 "Tõmba lesta!",
+                  "Sa oled nagu kapsas!" ],
+    "Surprise": [ "Tõsi või?",                         "Issand jumal!",               "Mida perset!",
+                   "Mida põrgut!",                     "Kuramus!",                    "Mind ei koti!",
+                   "Sitanikerdis!",                    "Kurivaim!",                   "Türaürask!",
+                   "Jubedus!" ],
+    "Too Far":  [ "Mul on nii nii nii kahju!",         "Suudlen su varbaid!",         "Tubli oled, mina olen praht!",
+                  "Anna andeks, ma olen täielik ahv!", "Ma olen täielik idioot!",     "Ma rooman su ees nagu uss!",
+                  "Las ma elan nagu su mopp!",         "Ma lähen laande häbi pärast!", "Palun andesta mu kanaaju!" ], 
+}
+
+
+# Copy of swear_dict to track unplayed swears
+
+unplayed_swears = {category: swear_dict[category][:] for category in swear_dict}
+
+# Function to translate Estonian swears to English
+
+def translate_swear(swear):
+    translations = {
+        # Anger
+        "Kurat!"                             : "Damn it!",
+        "Persevest!"                         : "Ass vest!",
+        "Sitapea!"                           : "Shithead!", 
+        "Käi põrgu!"                         : "Go to hell!",
+        "Loll kui saabas!"                   : "You're stupid like boot!",
+        "Kanaaju!"                           : "Chicken brain!",
+        "Jälkus!"                            : "Disgusting creature!",
+        "Tainapea!"                          : "Dough-head!",
+        "Loll nagu oinas!"                   : "You're stupid like ram",
+        "Käi kuradile!"                      : "Go to the devil!",
+        "Kuradi türa!"                       : "Devil cock!",
+        # Humor
+        "Püha müristus!"                     : "Holy thunder!",
+        "Taevas appi!"                       : "Heaven help us!",
+        "Püha püss!"                         : "Holy gun!",
+        "Mine metsa!"                        : "Go to the forest!",
+        "Sõida seenele!"                     : "Go pick mushrooms!",
+        "Tõmba uttu!"                        : "Pull into a fog!",
+        "Käi kuu peale!"                     : "Walk to the moon!",
+        "Käi kukele!"                        : "Go to the rooster!",
+        "Tõmba lesta!"                       : "Pull a flipper!",
+        "Sa oled nagu kapsas!"               : "You're like cabbage!",
+        # Surprise
+        "Tõsi või?"                          : "Is it true?",
+        "Issand jumal!"                      : "Lord God!",
+        "Mida perset!"                       : "What the ass!",
+        "Mida põrgut!"                       : "What the hell!",
+        "Kuramus!"                           : "Damnation!",
+        "Mind ei koti!"                      : "It doesn't bag me!",
+        "Sitanikerdis!"                      : "What a shit carving!",
+        "Kurivaim!"                          : "Angry spirits!",
+        "Türaürask!"                         : "Cockbeetles!",
+        "Jubedus!"                           : "What a mess!",
+        # Too Far
+        "Mul on nii nii nii kahju!"          : "I'm so so so sorry!",
+        "Suudlen su varbaid!"                : "I kiss your toes!",
+        "Tubli oled, mina olen praht!"       : "You are good, I am scum!",
+        "Anna andeks, ma olen täielik ahv!"  : "Forgive me, I am a monkey!",
+        "Ma olen täielik idioot!"            : "I'm a total idiot!",
+        "Ma rooman su ees nagu uss!"         : "I crawl before you like a worm!",
+        "Las ma elan nagu su mopp!"          : "Let me live as your mop!",
+        "Ma lähen laande häbi pärast!"       : "I'm going to the forest in shame!",
+        "Palun andesta mu kanaaju!"          : "Please forgive my chicken brain!",
+    }
+    
+    return translations.get(swear, "Unknown")
+
+
+def translate_file(swear):
+    translations = {
+        # Anger
+        "Kurat!"                             : "Kurat!",
+        "Persevest!"                         : "Persevest!",
+        "Sitapea!"                           : "Sitapea!", 
+        "Käi põrgu!"                         : "Kai porgu!",
+        "Loll kui saabas!"                   : "Loll kui saabas!",
+        "Kanaaju!"                           : "Kanaaju!",
+        "Jälkus!"                            : "Jalkus!",
+        "Tainapea!"                          : "Tainapea!",
+        "Loll nagu oinas!"                   : "Loll nagu oinas!",
+        "Käi kuradile!"                      : "Kai kuradile!",
+        "Kuradi türa!"                       : "Kuradi tura!",
+        # Humor
+        "Püha müristus!"                     : "Puha muristus!",
+        "Taevas appi!"                       : "Taevas appi!",
+        "Püha püss!"                         : "Puha puss!",
+        "Mine metsa!"                        : "Mine metsa!",
+        "Sõida seenele!"                     : "Soida seenele!",
+        "Tõmba uttu!"                        : "Tomba uttu!",
+        "Käi kuu peale!"                     : "Kai kuu peale!",
+        "Käi kukele!"                        : "Kai kukele!",
+        "Tõmba lesta!"                       : "Tomba lesta!",
+        "Sa oled nagu kapsas!"               : "Sa oled nagu kapsas!",
+        # Surprise
+        "Tõsi või?"                          : "Tosi voi?",
+        "Issand jumal!"                      : "Issand jumal!",
+        "Mida perset!"                       : "Mida perset!",
+        "Mida põrgut!"                       : "Mida porgut!",
+        "Kuramus!"                           : "Kuramus!",
+        "Mind ei koti!"                      : "Mind ei koti!",
+        "Sitanikerdis!"                      : "Sitanikerdis!",
+        "Kurivaim!"                          : "Kurivaim!",
+        "Türaürask!"                         : "Turaurask!",
+        "Jubedus!"                           : "Jubedus!",
+        # Too Far
+        "Mul on nii nii nii kahju!"          : "Mul on nii nii nii kahju!",
+        "Suudlen su varbaid!"                : "Suudlen su varbaid!",
+        "Tubli oled, mina olen praht!"       : "Tubli oled, mina olen praht!",
+        "Anna andeks, ma olen täielik ahv!"  : "Anna andeks, ma olen taielik ahv!",
+        "Ma olen täielik idioot!"            : "Ma olen taielik idioot!",
+        "Ma rooman su ees nagu uss!"         : "Ma rooman su ees nagu uss!",
+        "Las ma elan nagu su mopp!"          : "Las ma elan nagu su mopp!",
+        "Ma lähen laande häbi pärast!"       : "Ma lahen laande habi parast!",
+        "Palun andesta mu kanaaju!"          : "Palun andesta mu kanaaju!",
+    }
+    
+    return translations.get(swear, "Unknown")
+
+
+# Function to play a single swear without repeating
+
+def play_swear(category):
+    global unplayed_swears
+
+    if category == "Random":
+        category = random.choice(list(swear_dict.keys()))
+
+    # If all swears in this category have been played, reset the list
+
+    if not unplayed_swears[category]:
+        unplayed_swears[category] = swear_dict[category][:]
+
+    # Choose and remove a swear from the unplayed list
+
+    swear = random.choice(unplayed_swears[category])
+    unplayed_swears[category].remove(swear)
+
+    # Display and play it
+
+    swear_text.set(swear)
+    english_translation.set(translate_swear(swear))
+    play_audio(swear)
+
+
+# Function to play the intro message
+
+def play_intro():
+    audio_file = audio_dir + "/Tere tulemast Eesti Vandemasin!.mp3"
+    if os.path.exists(audio_file):
+        mixer.music.load(audio_file)
+        mixer.music.play()
+
+
+# Function to play audio
+
+def play_audio(swear):
+    audio_file = audio_dir + "/" + translate_file(swear) + ".mp3"
+    if os.path.exists(audio_file):
+        mixer.music.load(audio_file)
+        mixer.music.play()
+
+
+def play_combo():
+    import random
+
+    # Helper: lowercase & clean second swear
+    def to_combo_form(s):
+        s = s.strip()
+        if s.endswith(("!", "?", ".")):
+            s = s[:-1]
+        return s[0].lower() + s[1:]
+
+    # Collect all swears across all categories
+    #all_swears = [swear for cat in swear_dict.values() for swear in cat]
+
+    # Collect swears only from Anger, Humor, and Surprise
+    all_swears = [swear for cat_name, cat in swear_dict.items() if cat_name != "Too Far" for swear in cat]
+
+
+    # Pick two distinct swears
+    swear1, swear2 = random.sample(all_swears, 2)
+
+    # Build combo text (Estonian)
+    combo_est = f"{swear1[:-1]} {to_combo_form(swear2)}!"
+    swear_text.set(combo_est)
+
+    # Build translation text (English)
+    #combo_eng = f"{translate_swear(swear1)} {translate_swear(swear2).lower()}"
+    #english_translation.set(combo_eng)
+    eng1 = translate_swear(swear1).strip()
+    eng2 = translate_swear(swear2).strip()
+
+    # Remove trailing punctuation and lowercase second word
+    if eng1.endswith(("!", ".", "?")):
+        eng1 = eng1[:-1]
+    eng2 = eng2[0].lower() + eng2[1:]
+
+    combo_eng = f"{eng1} {eng2}!"
+    english_translation.set(combo_eng)
+
+
+    # Play audio sequentially — nice timing
+    root.after(1, lambda: mixer.music.load(audio_dir + "/" + translate_file(swear1) + ".mp3"))
+    root.after(2, lambda: mixer.music.play())
+    root.after(900, lambda: mixer.music.load(audio_dir + "/" + translate_file(swear2) + ".mp3"))  # Adjust timing if needed
+    root.after(901, lambda: mixer.music.play())
+
+#start new section1
+
+# --- Screen switching and shutdown helpers ---
+
+def show_main_screen():
+    """Rebuilds the main swearing screen."""
+    for widget in root.winfo_children():
+        widget.destroy()
+    build_main_screen()
+
+def show_menu_screen():
+    """Shows the Menu screen (Shutdown + Back)."""
+    for widget in root.winfo_children():
+        widget.destroy()
+    build_menu_screen()
+
+def shutdown_pi():
+    """Shutdown command (safe to test)."""
+    os.system("sudo shutdown -h now")
+    # During testing, replace above with:
+    # print("Shutdown pressed (disabled for testing)")
+
+#end new section 1
+
+#start new section 2
+
+# --- GUI setup ---
+
+def build_main_screen():
+    global bg_image
+
+    root.title("ESM Compact Mode")
+    root.attributes('-fullscreen', True)
+    root.config(cursor="none")
+
+    # Set size for 3.5” display
+    root.geometry("480x320")
+    root.configure(bg="black")
+
+    # Load background image (Estonian flag)
+    #bg_image = tk.PhotoImage(file="image/estonian_flag_small.png")
+    bg_image = tk.PhotoImage(file=image_dir+"/estonian_flag_small.png")
+    bg_label = tk.Label(root, image=bg_image)
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    # Variables for swear text and translation
+    global swear_text, english_translation
+    swear_text = tk.StringVar(value="")
+    english_translation = tk.StringVar(value="")
+
+    # Titles
+    estonian_title = "Eesti Vandemasin"
+    english_title = "Estonian Swearing Machine"
+
+    # --- Typewriter effect ---
+    def typewriter_effect(text, var, delay=50, index=0, callback=None):
+        if index < len(text):
+            var.set(text[:index + 1])
+            root.after(delay, typewriter_effect, text, var, delay, index + 1, callback)
+        elif callback:
+            root.after(300, callback)
+
+    def startup_sequence():
+        play_intro()
+        typewriter_effect(estonian_title, swear_text, delay=50, callback=lambda:
+            typewriter_effect(english_title, english_translation, delay=80))
+
+    root.after(1000, startup_sequence)
+
+    # Button style
+
+    button_style = {
+        "font": ("Arial", 14, "bold"),
+        "fg": "white",
+        "bg": "black",
+        "relief": "raised",
+        "bd": 4,
+        "width": 10,
+        "height": 2
+    }
+
+    # Create buttons
+
+    buttons = [
+        ( "Anger",    lambda: play_swear("Anger"), 1, 0),
+        ( "Humor",    lambda: play_swear("Humor"), 1, 1),
+        ( "Surprise", lambda: play_swear("Surprise"), 1, 2),
+        ( "Combo",    lambda: play_combo(), 2, 0),
+        ( "Too Far",  lambda: play_swear("Too Far"), 2, 1),
+        ( "Menu",     show_menu_screen, 2, 2)
+    ]
+
+    for text, command, row, col in buttons:
+        btn = tk.Button(root, text=text, command=command, **button_style)
+        btn.grid(row=row, column=col, padx=10, pady=10)
+
+        root.grid_columnconfigure(0, weight=1)
+        root.grid_columnconfigure(1, weight=1)
+        root.grid_columnconfigure(2, weight=1)
+
+    # Display Area
+    root.grid_rowconfigure(3,minsize=60)
+
+    tk.Label( root, textvariable=swear_text, font=("Arial", 32, "bold"),
+             fg="white", bg="black", justify="center").grid(row=10, column=0, columnspan=3, pady=5)
+
+    tk.Label(root, textvariable=english_translation, font=("Arial", 28, "italic"),
+             fg="white", bg="black", justify="center").grid(row=11, column=0, columnspan=3)
+
+#end new section 2
+
+#start new section 3
+
+def build_menu_screen():
+    global bg_image
+    #bg_image = tk.PhotoImage(file="image/estonian_flag_small.png")
+    bg_image = tk.PhotoImage(file=image_dir+"/estonian_flag_small.png")
+    bg_label = tk.Label(root, image=bg_image)
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    title = tk.Label(root, text="Menu", font=("Arial", 24, "bold"), bg="white")
+    title.pack(pady=20)
+
+    btn_style = {
+        "font": ("Arial", 16, "bold"),
+        "bg": "black",
+        "fg": "white",
+        "width": 12,
+        "height": 2
+    }
+
+    shutdown_btn = tk.Button(root, text="Shutdown", command=shutdown_pi, **btn_style)
+    shutdown_btn.pack(pady=10)
+
+    back_btn = tk.Button(root, text="Back", command=show_main_screen, **btn_style)
+    back_btn.pack(pady=10)
+
+#end new section 3
+
+#start new section 4
+
+build_main_screen()
+root.mainloop()
+
+#end new section 4
